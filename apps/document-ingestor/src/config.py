63a5a749363403928e1494f74cd5a0ca403916ce
Yoon Seok Seong <yoontjr@gmail.com>
2026-03-08 20:40:53 -0500

    fix(document-ingestor): ANTHROPIC_API_KEY fail-fast (os.environ[])

--- apps/document-ingestor/src/config.py
+++ apps/document-ingestor/src/config.py
@@ -1,5 +1,5 @@
 import os
 
-ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY", "")
+ANTHROPIC_API_KEY = os.environ["ANTHROPIC_API_KEY"]
 GOTENBERG_URL = os.getenv("GOTENBERG_URL", "http://gotenberg:3000")
 QUALITY_UPDATES_PATH = os.getenv("QUALITY_UPDATES_PATH", "/workspace/quality-updates")
